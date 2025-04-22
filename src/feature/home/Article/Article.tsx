c634513a1c0e71bffb6f49c14ed0c8a148c6804a
JungMin <wjdals0304@gmail.com>
2025-04-22 17:44:03 +0900

    feat: 반응형 적용

--- src/feature/home/Article/Article.tsx
+++ src/feature/home/Article/Article.tsx
@@ -26,7 +26,7 @@ export function Article() {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-5">
       <ArticleContent articles={articles} />
     </div>
   );
